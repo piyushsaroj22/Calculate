6bb02857250f5609565f79c4905078b6c5f9962a
Piyush Saroj <piyushsaroj222006@gmail.com>
2025-10-12 20:03:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function append(char) {
+  display.value += char;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
